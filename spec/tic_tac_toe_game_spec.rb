5386c535c10dc3d3cba20c68da57ca834bc391a6
Sherry He <xsherryhe@gmail.com>
2022-05-21 14:26:21 -0400

    Initiated tic_tac_toe_game_spec.rb to test Game class

---
+++ spec/tic_tac_toe_game_spec.rb
@@ -0,0 +1,116 @@
+require_relative '../tic_tac_toe.rb'
+describe TicTacToe::Game do
+  subject(:game) { described_class.new }
+
+  before do
+    allow(TicTacToe::Player)
+      .to receive(:new)
+      .and_return(instance_double(TicTacToe::Player, name: 'Joe', marker: 'X', number: 1),
+                  instance_double(TicTacToe::Player, name: 'Jane', marker: 'O', number: 2))
+  end
+
+  describe '#evaluate_game_over' do
+    context 'when the game is not over' do
+      before do
+        allow(TicTacToe::Board).to receive(:new).and_return [[' X ', ' O ', ' 3 '],
+                                                             [' O ', ' 5 ', ' 6 '],
+                                                             [' 7 ', ' 8 ', ' 9 ']]
+      end
+
+      it 'does not change @game_over to true' do
+        game.evaluate_game_over
+        game_over = game.instance_variable_get(:@game_over)
+        expect(game_over).not_to be true
+      end
+
+      it 'does not output a game-ending message' do
+        expect(game).not_to receive(:puts)
+        game.evaluate_game_over
+      end
+    end
+
+    context 'when a player wins horizontally' do
+      before do
+        allow(TicTacToe::Board).to receive(:new).and_return [[' X ', ' X ', ' X '],
+                                                             [' O ', ' 5 ', ' 6 '],
+                                                             [' 7 ', ' 8 ', ' 9 ']]
+        allow(game).to receive(:to_s)
+        allow(game).to receive(:puts)
+      end
+
+      it 'changes @game_over to true' do
+        game.evaluate_game_over
+        game_over = game.instance_variable_get(:@game_over)
+        expect(game_over).to be true
+      end
+
+      it "outputs message with the winning player's name" do
+        expect(game).to receive(:puts).with(/Joe has won the game!/)
+        game.evaluate_game_over
+      end
+    end
+
+    context 'when a player wins vertically' do
+      before do
+        allow(TicTacToe::Board).to receive(:new).and_return [[' 1 ', ' O ', ' X '],
+                                                             [' X ', ' O ', ' 6 '],
+                                                             [' X ', ' O ', ' 9 ']]
+        allow(game).to receive(:to_s)
+        allow(game).to receive(:puts)
+      end
+
+      it 'changes @game_over to true' do
+        game.evaluate_game_over
+        game_over = game.instance_variable_get(:@game_over)
+        expect(game_over).to be true
+      end
+
+      it "outputs message with the winning player's name" do
+        expect(game).to receive(:puts).with(/Jane has won the game!/)
+        game.evaluate_game_over
+      end
+    end
+
+    context 'when a player wins diagonally' do
+      before do
+        allow(TicTacToe::Board).to receive(:new).and_return [[' O ', ' 2 ', ' X '],
+                                                             [' X ', ' O ', ' 6 '],
+                                                             [' 7 ', ' X ', ' O ']]
+        allow(game).to receive(:to_s)
+        allow(game).to receive(:puts)
+      end
+
+      it 'changes @game_over to true' do
+        game.evaluate_game_over
+        game_over = game.instance_variable_get(:@game_over)
+        expect(game_over).to be true
+      end
+
+      it "outputs message with the winning player's name" do
+        expect(game).to receive(:puts).with(/Jane has won the game!/)
+        game.evaluate_game_over
+      end
+    end
+
+    context 'when the game ends on a tie' do
+      before do
+        allow(TicTacToe::Board).to receive(:new).and_return [[' O ', ' X ', ' X '],
+                                                             [' X ', ' O ', ' O '],
+                                                             [' O ', ' X ', ' X ']]
+        allow(game).to receive(:to_s)
+        allow(game).to receive(:puts)
+      end
+
+      it 'changes @game_over to true' do
+        game.evaluate_game_over
+        game_over = game.instance_variable_get(:@game_over)
+        expect(game_over).to be true
+      end
+
+      it 'outputs message with the tie result' do
+        expect(game).to receive(:puts).with(/Joe and Jane have tied./)
+        game.evaluate_game_over
+      end
+    end
+  end
+end
